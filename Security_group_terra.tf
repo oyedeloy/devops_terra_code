5b4106614145f183ca68c1f98230366f5d86df5f
dele <oyedeloy@gmail.com>
2020-09-28 22:15:23 -0500

    terraform code added

---
+++ Security_group_terra.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "demo_1" {
+  name = "first_tera_sec_group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ spin_tf.tf
@@ -0,0 +1,23 @@
+provider aws {
+  profile = "default"
+  region  = "us-east-2"
+}
+terraform {
+  backend "s3" {
+    bucket = "first-del-devops-bucket"
+    key    = "mtyfile.tfstate"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-04cd019702b8c27b8"
+  instance_type          = "t2.micro"
+  key_name               = "devops_key"
+  vpc_security_group_ids = [aws_security_group.demo_1.id]
+
+  tags = {
+    Name        = "First Terraform node"
+    Provisioner = "Terraform"
+  }
+}
